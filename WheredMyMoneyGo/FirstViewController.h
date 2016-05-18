8dd3c3b4ebedc899860b280af83dd18b218ea278
Rattletrap99 <timjones@tim-jones-macbook-pro.local>
2016-05-17 22:02:17 -0700

    3 random spaces

--- WheredMyMoneyGo/FirstViewController.h
+++ WheredMyMoneyGo/FirstViewController.h
@@ -3,7 +3,7 @@
 //  WheredMyMoneyGo
 //
 //  Created by Tim Jones on 2/5/15.
-//  Copyright (c) 2015 Tim Jones. All rights reserved.
+//  Copyright (c) 2015 Tim Jones. All rig   hts reserved.
 //
 
 #import <UIKit/UIKit.h>
